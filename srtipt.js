c66c1edfaea91e8eafd50bc39f787850be228a8f
mdjordje99 <97915375+mdjordje99@users.noreply.github.com>
2022-01-26 22:08:51 +0100

    Add files via upload

--- srtipt.js
+++ srtipt.js
@@ -1,10 +1,11 @@
 //Prompt unos --->
 let ime = prompt("Ime:");
 let prezime = prompt("Prezime:");
-let godinaRodjenja = prompt("Godina rođenja:");
-let trenutnaGodina = "2022";
+let godinaRodjenja = Number(prompt("Godina rođenja:"));
+let trenutnaGodina = 2022;
 let mestoRodjenja = prompt("Mesto rođenja:");
 let mestoBoravka = prompt("Na kom mestu trenutno boraviš?");
+let brojGodina = trenutnaGodina - godinaRodjenja;
 
-document.write("Moje ime je " + ime + "," + " a prezime mi je " + prezime + "." + "<br>" + "Ja imam " + (trenutnaGodina - godinaRodjenja) + " godina/godine." + "<br>" + 
-"Dolazim iz mesta " + mestoRodjenja + ", a trenutno boravim u mestu pod imenom " + mestoBoravka + ".");
+document.write(`Moje ime je ${ime}, a prezime mi je ${prezime}. <br> Ja imam ${brojGodina} godina/godine.  <br> 
+Dolazim iz mesta ${mestoRodjenja}, a trenutno boravim u mestu pod imenom ${mestoBoravka}.`);
